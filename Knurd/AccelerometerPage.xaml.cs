82dd151c6596c0549e51872d4c19f253b69cfb46
Iadgarov <david.iadgarov@gmail.com>
2016-12-17 14:57:39 +0200

    Add project files.

---
+++ Knurd/AccelerometerPage.xaml.cs
@@ -0,0 +1,117 @@
+﻿/*
+ * To add Offline Sync Support:
+ *  1) Add the NuGet package Microsoft.Azure.Mobile.Client.SQLiteStore (and dependencies) to all client projects
+ *  2) Uncomment the #define OFFLINE_SYNC_ENABLED
+ *
+ * For more information, see: http://go.microsoft.com/fwlink/?LinkId=717898
+ */
+//#define OFFLINE_SYNC_ENABLED
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.UI.Core;
+using Windows.Devices.Sensors;
+using Windows.UI;
+
+#if OFFLINE_SYNC_ENABLED
+using Microsoft.WindowsAzure.MobileServices.SQLiteStore;  // offline sync
+using Microsoft.WindowsAzure.MobileServices.Sync;         // offline sync
+#endif
+
+// for graphs: http://stackoverflow.com/questions/41190620/windows-universal-app-live-chart-of-accelerometer-data
+
+namespace Knurd
+{
+    public sealed partial class AccelerometerPage : Page
+    {
+
+
+
+        MyViewModel vm;
+
+        // Sensor and dispatcher variables
+        private Accelerometer _accelerometer;
+
+        // This event handler writes the current accelerometer reading to 
+        // the three acceleration text blocks on the app' s main page.
+
+        private async void ReadingChanged(object sender, AccelerometerReadingChangedEventArgs e)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                AccelerometerReading reading = e.Reading;
+                txtXAxis.Text = String.Format("{0,5:0.00}", reading.AccelerationX);
+                txtYAxis.Text = String.Format("{0,5:0.00}", reading.AccelerationY);
+                txtZAxis.Text = String.Format("{0,5:0.00}", reading.AccelerationZ);
+
+            });
+        }
+
+
+
+        public AccelerometerPage()
+        {
+            this.InitializeComponent();
+            _accelerometer = Accelerometer.GetDefault();
+
+            if (_accelerometer != null)
+            {
+                // Establish the report interval
+                uint minReportInterval = _accelerometer.MinimumReportInterval;
+                uint reportInterval = minReportInterval > 16 ? minReportInterval : 16;
+                _accelerometer.ReportInterval = reportInterval;
+
+                // Assign an event handler for the reading-changed event
+                _accelerometer.ReadingChanged += new TypedEventHandler<Accelerometer, AccelerometerReadingChangedEventArgs>(ReadingChanged);
+            }
+        }
+
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+#if OFFLINE_SYNC_ENABLED
+            await InitLocalStoreAsync(); // offline sync
+#endif
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            SystemNavigationManager.GetForCurrentView().BackRequested += MainPage_BackRequested;
+            base.OnNavigatedTo(e);
+
+            vm = new MyViewModel();
+            DataContext = vm;
+
+        }
+
+        private void MainPage_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (this.Frame.CanGoBack) this.Frame.GoBack();
+        }
+
+
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            vm.Start();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            vm.Stop();
+        }
+
+
+
+    }
+}
---
+++ Knurd/MyViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Devices.Sensors;
+using Windows.Foundation;
+using Windows.UI.Core;
+
+namespace Knurd
+{
+    public class MyViewModel
+    {
+        private Timer accelerometer;
+
+        private Random r;
+
+        private ObservableCollection<MyAccelModel> data;
+        public ObservableCollection<MyAccelModel> Data { get { return data; }  set { } }
+
+
+        // Sensor and dispatcher variables
+        private Accelerometer _accelerometer;
+
+        public MyViewModel()
+        {
+            data = new ObservableCollection<MyAccelModel>();
+
+            r = new Random(DateTime.Now.Millisecond);
+
+            _accelerometer = Accelerometer.GetDefault();
+
+            if (_accelerometer != null)
+            {
+                // Establish the report interval
+                uint minReportInterval = _accelerometer.MinimumReportInterval;
+                uint reportInterval = minReportInterval > 16 ? minReportInterval : 16;
+                _accelerometer.ReportInterval = reportInterval;
+
+                // Assign an event handler for the reading-changed event
+                //_accelerometer.ReadingChanged += new TypedEventHandler<Accelerometer, AccelerometerReadingChangedEventArgs>(ReadingChanged);
+            }
+        }
+
+        public void Start()
+        {
+            accelerometer = new Timer(AccelDataCallback, null, 100, 50); // last parameter is how often to take measurment, in mSec
+        }
+
+        public void Stop()
+        {
+            accelerometer.Dispose();
+        }
+
+        private async void AccelDataCallback(object state)
+        {
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                () =>
+                {
+                    if (_accelerometer != null)
+                    {
+                        AccelerometerReading reading = _accelerometer.GetCurrentReading();
+
+              
+
+
+                        data.Add(new MyAccelModel {
+                            TimeStamp = DateTime.Now,
+                            Accel = getAccelerationEnergy()
+                        });
+                    }
+                });
+        }
+
+        private double getAccelerationEnergy()
+        {
+            AccelerometerReading reading = _accelerometer.GetCurrentReading();
+            double temp = Math.Pow(Math.Pow(reading.AccelerationX, 2) + Math.Pow(reading.AccelerationY, 2) + Math.Pow(reading.AccelerationZ, 2), 0.5);
+            Debug.WriteLine(temp);
+            return temp;
+
+        }
+
+
+
+
+
+
+    }
+}
